c773f05701fa69cccf9827c39b5f802186de7787
tony <x232yu@uwaterloo.ca>
2017-04-01 18:46:32 +0000

    add empty st test

--- wlang/test_sym.py
+++ wlang/test_sym.py
@@ -47,7 +47,7 @@ class TestSym (unittest.TestCase):
         sym = wlang.sym.SymExec ()
         st = wlang.sym.SymState ()
         out = [s for s in sym.run (ast1, st)]
-        self.assertEquals (len(out), 1)
+        self.assertEquals (len(out), 11)
         
     def test_two_wrong (self):
         prg1 = "havoc x, y; assume y > 0; c := 0; r := x; while c < y inv c <= y do { r := r + 1; c := c + 1 }; assert r = x + y"
